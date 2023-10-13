29ad3e7b42ca0186b613bd34b0bf8e73b0a78fb2
ahmedqqp9 <mbbd3999@hotmail.com>
2023-10-13 11:23:17 -0700

    a

--- functions.c
+++ functions.c
@@ -165,7 +165,7 @@ int print_binary(va_list types, char buffer[],
 	UNUSED(precision);
 	UNUSED(size);
 
-	n = va_arg();
+	n = n = va_arg(types, unsigned int);
 	m = 2147483648; /* (2 ^ 31) */
 	a[0] = n / m;
 	for (i = 1; i < 32; i++)
